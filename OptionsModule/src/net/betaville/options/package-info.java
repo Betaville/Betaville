cff92f5d3aeb2c400ba99c3ff6e424cc64b5d436
Skye Book <skye.book@gmail.com>
2012-01-22 14:00:58 -0500

    Added options panel module

---
+++ OptionsModule/src/net/betaville/options/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@ContainerRegistration(id = "Betaville", categoryName = "#OptionsCategory_Name_Betaville", iconBase = "net/betaville/options/frame32.gif", keywords = "#OptionsCategory_Keywords_Betaville", keywordsCategory = "Betaville")
+@Messages(value = {"OptionsCategory_Name_Betaville=Betaville", "OptionsCategory_Keywords_Betaville=Betaville"})
+package net.betaville.options;
+
+import org.netbeans.spi.options.OptionsPanelController.ContainerRegistration;
+import org.openide.util.NbBundle.Messages;
