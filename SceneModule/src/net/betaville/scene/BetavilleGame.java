7bc913ff6089015f2060c4e5f174166420abd77c
Skye Book <skye.book@gmail.com>
2012-01-27 09:47:51 -0500

    Added picking listener in BetavilleGame.. not hooked up yet

--- SceneModule/src/net/betaville/scene/BetavilleGame.java
+++ SceneModule/src/net/betaville/scene/BetavilleGame.java
@@ -5,9 +5,13 @@
 package net.betaville.scene;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.collision.CollisionResults;
+import com.jme3.input.controls.AnalogListener;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
@@ -70,4 +74,41 @@ public class BetavilleGame extends SimpleApplication {
 
     @Override
     public void simpleUpdate(float tpf) {}
+    
+    private AnalogListener analogListener = new AnalogListener() {
+
+	public void onAnalog(String name, float intensity, float tpf) {
+	    if (name.equals("pick target")) {
+		// Reset results list.
+		CollisionResults results = new CollisionResults();
+		// Convert screen click to 3d position
+		Vector2f click2d = inputManager.getCursorPosition();
+		Vector3f click3d = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 0f).clone();
+		Vector3f dir = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(click3d);
+		// Aim the ray from the clicked spot forwards.
+		Ray ray = new Ray(click3d, dir);
+		// Collect intersections between ray and all nodes in results list.
+		rootNode.collideWith(ray, results);
+		// (Print the results so we see what is going on:)
+		for (int i = 0; i < results.size(); i++) {
+		    // (For each “hit”, we know distance, impact point, geometry.)
+		    float dist = results.getCollision(i).getDistance();
+		    Vector3f pt = results.getCollision(i).getContactPoint();
+		    String target = results.getCollision(i).getGeometry().getName();
+		    System.out.println("Selection #" + i + ": " + target + " at " + pt + ", " + dist + " WU away.");
+		}
+		// Use the results -- we rotate the selected geometry.
+		if (results.size() > 0) {
+		    // The closest result is the target that the player picked:
+		    Geometry target = results.getClosestCollision().getGeometry();
+		    // Here comes the action:
+		    if (target.getName().equals("Red Box")) {
+			target.rotate(0, -intensity, 0);
+		    } else if (target.getName().equals("Blue Box")) {
+			target.rotate(0, intensity, 0);
+		    }
+		}
+	    } // else if ...
+	}
+    };
 }
