514c2ccd700ff8d551152e86e9ca45f173ccc911
Skye Book <skye.book@gmail.com>
2012-01-29 14:36:23 -0500

    Right-Click now controls camera rotation

--- SceneModule/src/net/betaville/scene/CustomFlyByCamera.java
+++ SceneModule/src/net/betaville/scene/CustomFlyByCamera.java
@@ -6,7 +6,9 @@ package net.betaville.scene;
 
 import com.jme3.input.FlyByCamera;
 import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.renderer.Camera;
 
@@ -22,9 +24,14 @@ public class CustomFlyByCamera extends FlyByCamera{
     @Override
     public void registerWithInput(InputManager inputManager){
         super.registerWithInput(inputManager);
+	
+	this.inputManager.addMapping("AltLeft", new KeyTrigger(KeyInput.KEY_LMENU));
+	this.inputManager.addMapping("AltRight", new KeyTrigger(KeyInput.KEY_RMENU));
+	
         this.inputManager.deleteMapping("FLYCAM_RotateDrag");
-        this.inputManager.addMapping("FLYCAM_RotateDrag", new MouseButtonTrigger(MouseInput.BUTTON_MIDDLE));
-        this.inputManager.addListener(this, "FLYCAM_RotateDrag");
+        this.inputManager.addMapping("FLYCAM_RotateDrag", new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
+        this.inputManager.addListener(this, "FLYCAM_RotateDrag", "AltLeft", "AltRight");
+	
         setDragToRotate(true);
     }
     
