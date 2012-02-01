65a3bf2bae7067b187ac7101d4695e096d1c42c2
Skye Book <skye.book@gmail.com>
2012-02-01 15:31:24 -0500

    Fixes for messed up license headers

--- UserControlModule/src/net/betaville/usercontrol/CurrentLocationTopComponent.java
+++ UserControlModule/src/net/betaville/usercontrol/CurrentLocationTopComponent.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2008-2012, Brooklyn eXperimental Media Center All rights
- * reserved.
+ * Copyright (c) 2008-2012, Brooklyn eXperimental Media Center
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met: *
