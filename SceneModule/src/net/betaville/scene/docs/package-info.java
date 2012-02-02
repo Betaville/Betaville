58f9ac560c22a47158d643e0a494f1043097f5e8
Skye Book <skye.book@gmail.com>
2012-02-02 10:39:42 -0500

    Added JavaHelp files for the scene module

---
+++ SceneModule/src/net/betaville/scene/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "scene-hs.xml", position = 3651)
+package net.betaville.scene.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
