b5df11f83db9936c1691bd3d9a7b77f4885ba256
Skye Book <skye.book@gmail.com>
2012-02-15 21:36:51 -0500

    Bugfix in ProposalFetcher where it sends the city's ID rather than that of the proposal's root design

--- BetavilleApp/src/edu/poly/bxmc/betaville/model/ProposalFetcher.java
+++ BetavilleApp/src/edu/poly/bxmc/betaville/model/ProposalFetcher.java
@@ -27,12 +27,16 @@ public class ProposalFetcher {
 	
 	for(Design proposalRoot : proposalRoots){
 	    // Get the versions of the proposal
-	    List<Design> versions = icm.getVersionsOfProposal(proposalRoot.getCityID());
+	    List<Design> versions = icm.getVersionsOfProposal(proposalRoot.getID());
 	    
 	    // Create and populate a proposal chain
 	    ProposalChain chain = new ProposalChain(proposalRoot);
 	    for(Design version : versions){
 		chain.addVersion(version);
+	    }
+	    
+	    if(chain.getAllVersions().size()>0){
+		System.out.println(chain.getProposalRoot().getName() + " has " + chain.getAllVersions().size() + " versions");
 	    }
 	    
 	    chains.add(chain);
